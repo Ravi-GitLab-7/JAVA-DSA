fe5639db374ca6f244f86cdfed81b0e02c2ff926
Ravi Raj <msd7ravis@gmail.com>
2025-04-18 23:59:24 +0530

    Merge remote-tracking branch 'origin/main'

--- TwoDarray/TakeInput.java
+++ TwoDarray/TakeInput.java
@@ -1,4 +1,27 @@
 package TwoDarray;
 
+import java.util.Scanner;
+
 public class TakeInput {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[][] arr = new int[3][2];
+        System.out.println(arr.length);  // Number of rows
+        //input
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                arr[row][col] = in.nextInt();  // Read input correctly
+            }
+        }
+
+        // Optional: print array to confirm input
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                System.out.print(arr[row][col] + " ");
+            }
+            System.out.println();
+        }
+
+        in.close();
+    }
 }
