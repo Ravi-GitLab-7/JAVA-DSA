3cc6545d041a8cd9f5fd60c7dd963e89d63a439b
Ravi Raj <msd7ravis@gmail.com>
2025-04-28 21:45:17 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ Conditionals/ArmstrongNumber.java
@@ -0,0 +1,20 @@
+package Conditionals;
+
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        int num = 153, originalNum = num, sum = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            sum += digit * digit * digit;
+            num /= 10;
+        }
+
+        if (sum == originalNum) {
+            System.out.println(originalNum + " is an Armstrong number.");
+        } else {
+            System.out.println(originalNum + " is not an Armstrong number.");
+        }
+    }
+}
+
