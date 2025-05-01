ff003bd7d216a38727dcb6bad0412e0abe707dac
Ravi Raj <msd7ravis@gmail.com>
2025-05-01 23:46:29 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ Loops/EvenNumbers.java
@@ -0,0 +1,9 @@
+package Loops;
+
+public class EvenNumbers {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 50; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
