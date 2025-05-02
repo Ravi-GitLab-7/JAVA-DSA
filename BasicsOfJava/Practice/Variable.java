2062975d0aea94011d29567a5e8403f03d242ddc
Ravi Raj <msd7ravis@gmail.com>
2025-05-02 23:24:07 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ BasicsOfJava/Practice/Variable.java
@@ -0,0 +1,9 @@
+package BasicsOfJava.Practice;
+
+public class Variable {
+    public static void main(String[] args) {
+        int x = 6;
+        System.out.println(x);
+        System.out.print("x");
+    }
+}
