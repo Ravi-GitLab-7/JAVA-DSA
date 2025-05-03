7affef4cdabf786d0fa9f0ae1602bfb5c8b8a261
Ravi Raj <msd7ravis@gmail.com>
2025-05-03 19:32:41 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ BasicsOfJava/Practice/Arthmatic.java
@@ -0,0 +1,12 @@
+package BasicsOfJava.Practice;
+
+public class Arthmatic {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        System.out.println(a/b);
+        System.out.println(a%b);  //a%b=a (a<b)
+        System.out.println();
+
+    }
+}
