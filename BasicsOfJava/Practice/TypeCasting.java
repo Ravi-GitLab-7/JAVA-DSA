dcc7217151db0c88a1519ba2403a018a7363fa7c
Ravi Raj <msd7ravis@gmail.com>
2025-05-03 19:40:06 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ BasicsOfJava/Practice/TypeCasting.java
@@ -0,0 +1,10 @@
+package BasicsOfJava.Practice;
+import java.util.Scanner;
+public class TypeCasting {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        double y = (double) x;
+        System.out.println(y/2);
+    }
+}
