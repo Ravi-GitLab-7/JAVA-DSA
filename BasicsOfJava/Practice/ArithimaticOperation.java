0933d895bba92a1c45004e02cc974e8ca387bee7
Ravi Raj <msd7ravis@gmail.com>
2025-05-03 16:32:08 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ BasicsOfJava/Practice/ArithimaticOperation.java
@@ -0,0 +1,12 @@
+package BasicsOfJava.Practice;
+
+public class ArithimaticOperation {
+    public static void main(String[] args) {
+        int  x = 5, y = 10;
+        System.out.println(x+y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x % y);
+        System.out.println(x / y);
+    }
+}
---
+++ BasicsOfJava/Practice/DaubleDAtaTYpe.java
@@ -0,0 +1,4 @@
+package BasicsOfJava.Practice;
+
+public class DaubleDAtaTYpe {
+}
