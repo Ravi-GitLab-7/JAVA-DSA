65ee9d75eb3d7ca344099f621507f30d14c5702e
Ravi Raj <msd7ravis@gmail.com>
2025-05-06 14:29:15 +0530

    Merge remote-tracking branch 'origin/main'

--- PatternPrinting/Practice/BinaryTriangle.java
+++ PatternPrinting/Practice/BinaryTriangle.java
@@ -10,10 +10,9 @@ public class BinaryTriangle {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
                 // Alternate between 1 and 0 based on (i + j) % 2
-                if ((i + j) % 2 == 0)
-                    System.out.print("1 ");
-                else
-                    System.out.print("0 ");
+//
+                if((i+j)%2==0) System.out.print("1"+" ");
+                else System.out.print("0"+" ");
             }
             System.out.println(); // New line after each row
         }
