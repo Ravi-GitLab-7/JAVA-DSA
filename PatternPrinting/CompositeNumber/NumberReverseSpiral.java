fc661a1d43061572cf1a5cdb57b7c95796d40e9c
Ravi Raj <msd7ravis@gmail.com>
2025-05-08 12:57:24 +0530

    Merge remote-tracking branch 'origin/main'

--- PatternPrinting/CompositeNumber/NumberReverseSpiral.java
+++ PatternPrinting/CompositeNumber/NumberReverseSpiral.java
@@ -6,9 +6,10 @@ public class NumberReverseSpiral {
             for (int j = 1; j <=2*n-1  ; j++) {
                 int a=i,b=j;
                 if(i>n) a= 2*n-i;
-                if(j>n) b= 2*n-j;
+                if (j>n) b= 2*n-j;
                 System.out.print(n+1-Math.min(a,b)+" ");
             }
+
             System.out.println();
         }
     }
