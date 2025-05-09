e9aa2cb74b4161df67f37128f2bbe6f3a77b7f17
Ravi Raj <msd7ravis@gmail.com>
2025-05-09 19:06:45 +0530

    Merge remote-tracking branch 'origin/main'

--- OOPS/TestApptwo.java
+++ OOPS/TestApptwo.java
@@ -1,17 +1,23 @@
 package OOPS;
 
 public class TestApptwo {
-    class person {  //base class
+//    class person {  //base class
+//        public String name;
+//        public String address;
+//        public int age;
+//    }
+
+    class student  {
+        //instance variable
         public String name;
         public String address;
         public int age;
-    }
-
-    class student extends person {
         public int marks;
         public String grade;
 
+        //constructor
         student(String name, String address, int age, int marks, String grade) {
+            System.out.println("called during Object Creation...");
             this.name = name;
             this.address = address;
             this.age = age;
@@ -19,7 +25,8 @@ public class TestApptwo {
             this.grade = grade;
         }
 
-        public void disp() {  //constructor
+        //normal methods
+        public void disp() {
             System.out.println("Name is : " + name);
             System.out.println("Address is : " + address);
             System.out.println("Age is : " + age);
