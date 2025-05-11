51c88d0d1d6c12d48d85f9b4ccafd15eba46bbce
Ravi Raj <msd7ravis@gmail.com>
2025-05-11 18:12:57 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ OOPS/MethodOneButDifferent.java
@@ -0,0 +1,17 @@
+package OOPS;
+
+public class MethodOneButDifferent{
+    public void mehtodOne(String s){
+        System.out.println("String Buffer o");
+    }
+    public void mehtodOne(Object o){
+        System.out.println("Object version...");
+    }
+
+    public static void main(String[] args) {
+        MethodOne m=new MethodOne();
+        m.mehtodOne("Ravi");  // String -->String
+        m.mehtodOne(new Object()); //Object -->Object
+        m.mehtodOne(null);  //null -->String reference  Object reference
+    }
+}
