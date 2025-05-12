6c918f6cd58e3b7dacf5777ebc2822d2b2d2337d
Ravi Raj <msd7ravis@gmail.com>
2025-05-12 23:51:25 +0530

    Merge remote-tracking branch 'origin/main'

--- OOPS/OVERRIDING/test.java
+++ OOPS/OVERRIDING/test.java
@@ -1,11 +1,20 @@
 package OOPS.OVERRIDING;
-
-
+import org.w3c.dom.ls.LSOutput;
 public class test {
     class parent {
-
+        public void property(){
+            System.out.println("Land+Cash+Gold");
+        }
+        public void merry(){
+            System.out.println("Raelativegirl...");
+        }
+    }
+    class child extends parent{
+        public void merry() {
+            System.out.println("Look AnotherGirl..");
+        }
     }
     public static void main(String[] args) {
-
+       parent p1 =  new parent();
     }
 }
