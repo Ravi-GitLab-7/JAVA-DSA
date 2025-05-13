42f201f10e51affc3b7e50f3f3e662b6778f99b3
Ravi Raj <msd7ravis@gmail.com>
2025-05-13 22:30:16 +0530

    Merge remote-tracking branch 'origin/main'

--- OOPS/OVERRIDING/test3.java
+++ OOPS/OVERRIDING/test3.java
@@ -5,14 +5,12 @@ class Parent {
         return null;
     }
 }
-
 class Child extends Parent {
     public Object methodOne() { // Signature matches the parent method
         System.out.println("Hello From child");
         return null; // Return a value as the return type is Object
     }
 }
-
 public class test3 {
     public static void main(String[] args) {
         Parent p = new Child();
