f58fcfabb9d9bd0fee15c2626183b431311b3706
Ravi Raj <msd7ravis@gmail.com>
2025-05-13 14:33:21 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ OOPS/OVERRIDING/Aeroplane.java
@@ -0,0 +1,16 @@
+package OOPS.OVERRIDING;
+
+public class Aeroplane {
+    public void takeOff(){
+        System.out.println("Taking off...");
+    }
+    public void land(){
+        System.out.println( "Landing...");
+    }
+    public void fly(){
+        System.out.println("Flying...");
+    }
+    public static void main(String[] args) {
+
+    }
+}
