da302b43b4ecf7a12756c495fd056ced17a47e84
Ravi Raj <msd7ravis@gmail.com>
2025-05-13 16:02:21 +0530

    Merge remote-tracking branch 'origin/main'

--- OOPS/OVERRIDING/Aeroplane.java
+++ OOPS/OVERRIDING/Aeroplane.java
@@ -10,7 +10,78 @@ public class Aeroplane {
     public void fly(){
         System.out.println("Flying...");
     }
-    public static void main(String[] args) {
 
+    class passengerPlane extends Aeroplane{
+        public void takeOff(){
+            System.out.println("Passenger-Plan is Taking off...");
+        }
+        public void land(){
+            System.out.println( "Passenger-Plan is Landing...");
+        }
+        public void fly(){
+            System.out.println("Passenger-Plan is Flying...");
+        }
+    }
+    class cargoPlane extends Aeroplane{
+        public void takeOff(){
+            System.out.println("Cargo-Plan is Taking off...");
+        }
+        public void land(){
+            System.out.println( "Cargo-Plan is Landing...");
+        }
+        public void fly(){
+            System.out.println("Cargo-Plan is Flying...");
+        }
+    }
+    class fighterPlane extends Aeroplane{
+        public void takeOff(){
+            System.out.println("Fighter-Plan is Taking off...");
+        }
+        public void land(){
+            System.out.println( "Fighter-Plan is Landing...");
+        }
+        public void fly(){
+            System.out.println( "Fighter-Plan is flying...");
+        }
+    }
+
+    class airport extends Aeroplane{
+        public void allowAeroplane(Aeroplane ap){
+            ap.takeOff();
+            ap.fly();
+            ap.land();
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Aeroplane outer = new Aeroplane();
+//        Aeroplane ap = null;
+//        ap = outer.new passengerPlane();
+//
+//        ap.takeOff();
+//        ap.fly();
+//        ap.land();
+//
+//        System.out.println();
+//
+//        ap = outer.new cargoPlane();
+//        ap.takeOff();
+//        ap.fly();
+//        ap.land();
+//
+//        System.out.println();
+//
+//        ap = outer.new fighterPlane();
+//        ap.takeOff();
+//        ap.fly();
+//        ap.land();
+        passengerPlane ap = outer.new passengerPlane();
+        cargoPlane cp = outer.new cargoPlane();
+        fighterPlane fp = outer.new fighterPlane();
+
+        airport a = outer.new airport();
+        a. allowAeroplane(ap);
+        a. allowAeroplane(cp);
+        a. allowAeroplane(fp);
     }
 }
