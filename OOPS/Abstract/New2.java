2686141d043ea5962cd0c78ab517c578b28b956d
Ravi Raj <msd7ravis@gmail.com>
2025-05-14 23:48:51 +0530

    Merge remote-tracking branch 'origin/main'

--- OOPS/Abstract/New2.java
+++ OOPS/Abstract/New2.java
@@ -1,5 +1,32 @@
 package OOPS.Abstract;
+abstract class Birds{
+  public abstract void fly();
+  public abstract void eat();
+}
+class Sparrow extends Birds{
+    public void fly() {
 
+    }
+    public void eat() {
+
+    }
+}
+class Eagle extends Birds{
+    public void fly() {
+
+    }
+    public void eat() {
+
+    }
+}
+class Crow extends Birds{
+    public void fly() {
+
+    }
+    public void eat() {
+
+    }
+}
 public class New2 {
     public static void main(String[] args) {
 
