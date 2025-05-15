62620dfed073cb786a7021b6d6a0a1f8e701a290
Ravi Raj <msd7ravis@gmail.com>
2025-05-15 22:10:25 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ OOPS/Abstract/Iterface/test.java
@@ -0,0 +1,13 @@
+package OOPS.Abstract.Iterface;
+interface ICalculator{
+    //public abstract
+    void add(int  a, int b);
+    void sub(int a, int b);
+    void mul(int a, int b);
+    void div(int a, int b);
+}
+public class test {
+    public static void main(String[] args) {
+
+    }
+}
