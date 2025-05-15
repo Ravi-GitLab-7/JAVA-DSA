31f25c5942d95b0327ac2cbe5ad8fd9385b1bedb
Ravi Raj <msd7ravis@gmail.com>
2025-05-15 22:32:48 +0530

    Merge remote-tracking branch 'origin/main'

--- OOPS/Abstract/Iterface/test.java
+++ OOPS/Abstract/Iterface/test.java
@@ -1,13 +1,32 @@
-package OOPS.Abstract.Iterface;
-interface ICalculator{
-    //public abstract
-    void add(int  a, int b);
-    void sub(int a, int b);
-    void mul(int a, int b);
-    void div(int a, int b);
-}
-public class test {
-    public static void main(String[] args) {
-
-    }
-}
+//package OOPS.Abstract.Iterface;
+//interface ICalculator{
+//    //public abstract  //abstract method
+//    void add(int  a, int b);
+//    void sub(int a, int b);
+//    void mul(int a, int b);
+//    void div(int a, int b);
+//}
+//interface CalculatorImpl implements ICalculator{
+//    public void add(int a, int b){
+//        System.out.println("sum of two numbers is: "+(a+b));
+//    }
+//    public void sub(int a, int b){
+//        System.out.println("sub of two numbers is: "+(a-b));
+//    }
+//    public void mul(int a, int b){
+//        System.out.println("mul of two numbers is: "+(a*b));
+//    }
+//    public void div(int a, int b){
+//        System.out.println("div of two numbers is: "+(a/b));
+//    }
+//}
+//public class test {
+//    public static void main(String[] args) {
+//        //loose coupling :: polymorphism
+//        ICalculator calculator = new CalculatorImpl();
+//        calculator.add(10, 20);
+//        calculator.sub(10, 20);
+//        calculator.mul(10, 20);
+//        calculator.div(10, 20);
+//    }
+//}
