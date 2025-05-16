1b529cd494c3d1add18e7cc27a1b8bb88db144aa
Ravi Raj <msd7ravis@gmail.com>
2025-05-16 23:26:56 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ Array/BasicSyntax.java
@@ -0,0 +1,11 @@
+package Array;
+
+public class BasicSyntax {
+    public static void main(String[] args) {
+        int x=5;
+        int []arr ={2,3,4,5,6};
+        for (int i = 0; i <5 ; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
