7b242a6a6e0584b3c7d6bc2a96bcdae012f255f6
Ravi Raj <msd7ravis@gmail.com>
2025-05-16 15:25:23 +0530

    Merge remote-tracking branch 'origin/main'

--- OOPS/Instance/test1.java
+++ OOPS/Instance/test1.java
@@ -1,11 +1,13 @@
 package OOPS.Instance;
 // Public Static final
-interface IDemo
+interface IDemo  //interface name
 {
     int x = 10;
 }
 public class test1 implements IDemo{
     public static void main(String[] args) {
-
+        int x =100; //local variable
+        System.out.println(x);  //in which more priority of local than static
+        System.out.println(IDemo.x);  //using interface name
     }
 }
