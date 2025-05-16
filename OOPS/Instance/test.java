1b27cb0bba6a356fc310583973bf7bf507813ad4
Ravi Raj <msd7ravis@gmail.com>
2025-05-16 15:21:20 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ OOPS/Instance/test.java
@@ -0,0 +1,11 @@
+package OOPS.Instance;
+interface IRemote{
+    //public static final
+    int MIN_VALUE=0;
+    int MAX_VALUE=100;
+}
+public class test {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ OOPS/Instance/test1.java
@@ -0,0 +1,11 @@
+package OOPS.Instance;
+// Public Static final
+interface IDemo
+{
+    int x = 10;
+}
+public class test1 implements IDemo{
+    public static void main(String[] args) {
+
+    }
+}
