ca1930bd2055ebe44992b7ce61d57c2a678c3094
Ravi Raj <msd7ravis@gmail.com>
2025-05-18 23:35:42 +0530

    Shallow copy , It Means don't copy actually it will be shadow copy of the array it will be happining only name change

---
+++ Array/CopyArray.java
@@ -0,0 +1,14 @@
+package Array;
+
+public class CopyArray {
+    public static void main(String[] args) {
+        int []arr = {23,34,45,67,23,34};
+        for (int ele : arr){
+            System.out.print(ele+" ");
+        }
+        System.out.println();
+        int [] nums = arr;
+        nums[0]=100;
+        System.out.println(nums[0]);
+    }
+}
