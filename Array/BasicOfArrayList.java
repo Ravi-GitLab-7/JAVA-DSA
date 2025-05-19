ed76a0b8e90625fde3fc8d1883c9ab42726f81c8
Ravi Raj <msd7ravis@gmail.com>
2025-05-19 22:17:49 +0530

    ArrayList add element

---
+++ Array/BasicOfArrayList.java
@@ -0,0 +1,7 @@
+package Array;
+import java.util.ArrayList;
+public class MyArrayListExample {
+    public static void main(String[] args) {
+        MyArrayListExample<Integer> arr = new MyArrayListExample<>(5);
+    }
+}
