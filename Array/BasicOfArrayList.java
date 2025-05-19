a40722dd4e1459bc8e9dc83e47e01d3e3f5a39e0
Ravi Raj <msd7ravis@gmail.com>
2025-05-19 22:45:54 +0530

    ArrayList add element

--- Array/BasicOfArrayList.java
+++ Array/BasicOfArrayList.java
@@ -12,7 +12,7 @@ public class BasicOfArrayList {
         arr.add(3,40);  
         arr.add(4,50);  
         arr.add(5,60);
-//        for (int i = 0; i <6 ; i++) {
+//        for (int i = 0; i <6 ; i++) {  // arr.size()
 //            System.out.print(arr.get(i)+" "); //add new element
 //        }
         System.out.println(arr);
@@ -22,9 +22,12 @@ public class BasicOfArrayList {
 //          for(int ele : arr){
 //              System.out.print(ele+" ");
 //          }
-                    for (int j = 0; j <6 ; j++) {
+                    for (int j = 0; j <6 ; j++) {  //arr.size()
             System.out.print(arr.get(j)+" ");
-        }
+            }
+            System.out.println();
+             arr.add(90);  // push back
+            System.out.print(arr);
         }
     }
 }
