fdc2865b375393bd955a483f138f9dd4395169a2
Ravi Raj <msd7ravis@gmail.com>
2025-05-19 23:53:30 +0530

    Add two element is eql to target no.

---
+++ Array/Doublet.java
@@ -0,0 +1,16 @@
+package Array;
+
+public class Doublet {
+    public static void main(String[] args) {
+        int x = 9;
+        int [] arr = {2,4,5,8,9,7};
+        for (int i = 0; i < arr.length ; i++) {
+             for (int j = i+1; j < arr.length ; j++) {
+                 if(arr[i] + arr[j] == x ){
+                     System.out.println(arr[i]+" "+arr[j]);
+                     break;
+                 }
+             }
+        }
+    }
+}
