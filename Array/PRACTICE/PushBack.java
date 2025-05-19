0fb3e9c2481bf41d2496a8808d53325bd83379ea
Ravi Raj <msd7ravis@gmail.com>
2025-05-19 23:01:37 +0530

    push back element & Remove Element through index

--- Array/PRACTICE/PushBack.java
+++ Array/PRACTICE/PushBack.java
@@ -3,18 +3,16 @@ import java.util.ArrayList;
 public class PushBack {
     public static void main(String[] args) {
         ArrayList<Integer>arr = new ArrayList<>(5);
-        arr.add(10);
+        System.out.println(arr+" "+arr.size());
         arr.add(20);
+        System.out.println(arr+" "+arr.size());
         arr.add(30);
-        arr.add(40);
+        System.out.println(arr+" "+arr.size());
+        arr.add(-40);
+        System.out.println(arr+" "+arr.size());
         arr.add(50);
-        for (int i = 0; i <arr.size() ; i++) {
-            System.out.print(arr);
-        }
-        System.out.println();
-        arr.add(60);
-        for (int i = 0; i <arr.size() ; i++) {
-            System.out.print(arr);
-        }
+        System.out.println(arr+" "+arr.size());
+        arr.remove(2);
+        System.out.println(arr+" "+arr.size());
     }
 }
