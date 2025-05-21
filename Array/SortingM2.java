4de188c779d30023386f00251a8b8b57c9a2e24a
Ravi Raj <msd7ravis@gmail.com>
2025-05-21 22:30:12 +0530

    sorting array by swapping method

--- Array/SortingM2.java
+++ Array/SortingM2.java
@@ -2,10 +2,21 @@ package Array;
 
 public class SortingM2 {
     public static void main(String[] args) {
-        int i = 0, j = 1;
-        int [] arr = {0,1,0,1,1,0,0,1,1,0,1};
+        int [] arr = {1,0,0,0,1,1,1};
         int n = arr.length;
-
-
+        int i = 0, j = n -  1;
+        while (i < j) {
+            if(arr[i]==0) i++;
+            if(arr[j]==1) j--;
+            if(i>j) break;
+            if(arr[i]==1 && arr[j]==0){
+                arr[i] = 0;
+                arr[j]=1;
+                i++;
+                j--;
+            }
+        }
+        for(int ele : arr)
+            System.out.print(ele+" ");
     }
 }
