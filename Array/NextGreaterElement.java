984ce2d7c70050ad1dc6835993ff34cc9f68df15
Ravi Raj <msd7ravis@gmail.com>
2025-05-23 19:32:25 +0530

    find the next greater element

--- Array/NextGreaterElement.java
+++ Array/NextGreaterElement.java
@@ -2,23 +2,27 @@ package Array;
 
 public class NextGreaterElement {
     public static void main(String[] args) {
-        int [] arr ={10,20,34,56,23,6,67,23,78,34};
+        int [] arr = {10, 20, 34, 56, 23, 6, 67, 23, 78, 34};
         int n = arr.length;
-        int [] ans = new int [n];
-        ans[n-1] = -1;
-        for(int i=0;i<n;i++){
-            int max = Integer.MIN_VALUE;
-            for(int j= i+1;j<n;j++){
-                max = Math.max(max,arr[j]);
-            }
-            ans[i] = max;
+        int [] ans = new int[n];
+        
+        // Explicitly set the last element of ans
+        ans[n-1] = -1; // Or any default value you want
+        
+        // Optimized Next Greater Element logic
+        int nge = arr[n-1];
+        for (int i = n-2; i >= 0; i--) {
+            ans[i] = nge;
+            nge = Math.max(nge, arr[i]);
         }
-        for(int ele : arr){
-            System.out.print(ele+" ");
+
+        // Printing the array content
+        for (int ele : arr) {
+            System.out.print(ele + " ");
         }
         System.out.println();
-        for(int ele : ans){
-            System.out.print(ele+" ");
+        for (int ele : ans) {
+            System.out.print(ele + " ");
         }
     }
 }
