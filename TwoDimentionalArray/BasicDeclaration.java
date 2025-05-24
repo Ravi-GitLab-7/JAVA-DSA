18e554640bc2199af27f624858c24bfae6c48478
Ravi Raj <msd7ravis@gmail.com>
2025-05-24 18:33:05 +0530

    taking input in 2D array and also find rows and colns

--- TwoDimentionalArray/BasicDeclaration.java
+++ TwoDimentionalArray/BasicDeclaration.java
@@ -4,16 +4,20 @@ public class BasicDeclaration {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int [][]arr = new int [2][3];
+        int m = arr.length;
+        int n = arr[0].length;
+//        System.out.println(m+" ");  //find numbers of rows
+//        System.out.println(n+" ");  //find numbers of columns
         // input of 2D array
-        for (int i = 0; i <2 ; i++) {    //for rows
-            for (int j = 0; j <3 ; j++) {  //for columns
+        for (int i = 0; i <m ; i++) {    //for rows
+            for (int j = 0; j <n ; j++) {  //for columns
               arr [i][j] = sc.nextInt();
             }
         }
 
-        //output of two dimensional array
-        for (int i = 0; i <2 ; i++) {    //for rows
-            for (int j = 0; j <3 ; j++) {  //for columns
+       // output of two dimensional array
+        for (int i = 0; i <m ; i++) {    //for rows
+            for (int j = 0; j <n ; j++) {  //for columns
                 System.out.print(arr[i][j]+" ");
             }
             System.out.println();
