b3288c65cef38ff9f1a1da572460344d1e42857b
Ravi Raj <msd7ravis@gmail.com>
2025-05-26 17:22:22 +0530

    MergeaTwoSoted

---
+++ Array/PRACTICE/MergeaTwoSoted.java
@@ -0,0 +1,38 @@
+package Array.PRACTICE;
+
+public class MergeaTwoSoted {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 5};
+        int[] b = {2, 3, 4, 5, 6};
+        int[] c = new int[a.length + b.length];
+        int i = 0, j = 0, k = 0;
+
+        // Merging two sorted arrays
+        while (i < a.length && j < b.length) {
+            if (a[i] < b[j]) {
+                c[k] = a[i];
+                i++;
+            } else {
+                c[k] = b[j];
+                j++;
+            }
+            k++;
+        }
+// last edges
+        while (i < a.length) {
+            c[k] = a[i];
+            i++;
+            k++;
+        }
+
+        while (j < b.length) {
+            c[k] = b[j];
+            j++;
+            k++;
+        }
+
+        for (int ele : c) {
+            System.out.print(ele + " ");
+        }
+    }
+}
