a28bfb7ba28f982f9e69ae141dc05da295dbfbdb
Ravi Raj <msd7ravis@gmail.com>
2025-05-30 22:58:05 +0530

    DoubleData

---
+++ BasicsOfJava/DoubleData.java
@@ -0,0 +1,15 @@
+package BasicsOfJava;
+
+public class DoubleData {
+    public static void main(String[] args) {
+        double x=4;
+        double y = 5;
+        double z =6;
+        double sum = x+y+z;
+        System.out.println(sum);
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+    }
+}
