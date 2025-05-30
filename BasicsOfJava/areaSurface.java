1ae0dcaef7378b3b339e7f08eb3cbf026d656fea
Ravi Raj <msd7ravis@gmail.com>
2025-05-30 23:01:30 +0530

    areaSurface

---
+++ BasicsOfJava/areaSurface.java
@@ -0,0 +1,9 @@
+package BasicsOfJava;
+
+public class areaSurface {
+    public static void main(String[] args) {
+        double radius = 5;
+        double area = 3.14 * radius * radius;
+        System.out.println("Area of surface is :"+area);
+    }
+}
