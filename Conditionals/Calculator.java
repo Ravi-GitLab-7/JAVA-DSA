8a900d6aa018fa0ea11d72fd1e3a152b0fbddb86
Ravi Raj <msd7ravis@gmail.com>
2025-05-31 21:45:49 +0530

    Calculator

---
+++ Conditionals/Calculator.java
@@ -0,0 +1,47 @@
+package Conditionals;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int number = -5;
+        if (number > 0) {
+            System.out.println("Number is positive.");
+        } else if (number < 0) {
+            System.out.println("Number is negative.");
+        } else {
+            System.out.println("Number is zero.");
+        }
+
+        // 2. switch condition
+        int day = 4;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid day");
+        }
+
+        // 3. Ternary operator
+        int a = 15, b = 20;
+        String result = (a > b) ? "a is greater" : "b is greater";
+        System.out.println(result);
+    }
+}
