dbdc8b28f8f9491fb86f2bd4e9b4154a228f1014
Ravi Raj <msd7ravis@gmail.com>
2025-06-02 18:06:56 +0530

    storing with the help of new transpose array

--- TwoDimentionalArray/TransposeOfMAtrix.java
+++ TwoDimentionalArray/TransposeOfMAtrix.java
@@ -3,9 +3,20 @@ public class TransposeOfMAtrix {
     public static void main(String[] args) {
         int [][] arr = {{2,3,4},{3,4,5}};
         int m = arr.length ,n = arr[0].length;
-        for (int j = 0; j <n ; j++) {
-            for (int i = 0; i <m ; i++) {
-                System.out.print(arr[i][j]+" ");
+        // coln wise print
+//        for (int j = 0; j <n ; j++) {  // colns
+//            for (int i = 0; i <m ; i++) {  // rows
+//                System.out.print(arr[i][j]+" ");
+//            }
+//            System.out.println();
+//        }
+
+        // storing with the help of new transpose array
+        int [][]transpose = new int [n][m];
+        for (int i = 0; i <n ; i++) {
+            for (int j = 0; j <m ; j++) {
+                transpose[j][i] = arr[i][j];
+                System.out.print(transpose[i][j]+" ");
             }
             System.out.println();
         }
