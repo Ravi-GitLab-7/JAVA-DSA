f673dc9afee6c6ad4e3ae7d84c55537bb07fb667
Ravi Raj <msd7ravis@gmail.com>
2025-06-06 19:36:29 +0530

    Transpose

---
+++ TwoDimentionalArray/Practice/Transpose.java
@@ -0,0 +1,25 @@
+package TwoDimentionalArray.Practice;
+
+public class Transpose {
+    public static void main(String[] args) {
+        int[][] arr = {{2, 4, 1}, {-10, 5, 11}, {18, -7, 6}};
+        int m = arr.length, n = arr[0].length;
+        int[][] transpose = new int[n][m];
+
+        // Compute the transpose
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                transpose[j][i] = arr[i][j];
+            }
+        }
+
+        // Print the transposed matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(transpose[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
