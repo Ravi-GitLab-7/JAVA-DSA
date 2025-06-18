f710370371d35a9f73fc9edb71a6990dd984494a
Ravi Raj <msd7ravis@gmail.com>
2025-06-18 21:56:22 +0530

    Declaration

---
+++ TwoDimentionalArray/Practice/Declaration.java
@@ -0,0 +1,27 @@
+package TwoDimentionalArray.Practice;
+import java.util.Scanner;
+public class Declaration {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int [][]arr = new int [2][3];
+        int m = arr.length;
+        int n = arr[0].length;
+//        System.out.println(m+" ");  //find numbers of rows
+//        System.out.println(n+" ");  //find numbers of columns
+        // input of 2D array
+        for (int i = 0; i <m ; i++) {    //for rows
+            for (int j = 0; j <n ; j++) {  //for columns
+                arr [i][j] = sc.nextInt();
+            }
+        }
+
+        // output of two dimensional array
+        for (int i = 0; i <m ; i++) {    //for rows
+            for (int j = 0; j <n ; j++) {  //for columns
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
