4a44c8fe0126acb8e93376dae34b25218ab80e23
Ravi Raj <msd7ravis@gmail.com>
2025-06-22 21:42:06 +0530

    Question1

---
+++ Array/PRACTICE/Question1.java
@@ -0,0 +1,13 @@
+package Array.PRACTICE;
+
+public class Question1 {
+    public static void main(String[] args) {
+        int []arr = {23,34,45,67,23,34};
+        int n = arr.length;
+        for (int i = 0; i <=n ; i++) {
+            if(arr[i]<35){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
