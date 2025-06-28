1870198ee04724d92ac83a3acb428e263d1c77f1
Ravi Raj <msd7ravis@gmail.com>
2025-06-28 23:11:06 +0530

    ForEachLoop2

--- Array/ForEachLoop2.java
+++ Array/ForEachLoop2.java
@@ -1,7 +1,5 @@
 package Array;
-
 import java.util.Arrays;
-
 public class ForEachLoop2 {
     public static void main(String[] args) {
         int[] arr = {23,50,90,4,105, 34, 45, 23, 34};
@@ -9,7 +7,6 @@ public class ForEachLoop2 {
         for(int ele : arr){
             System.out.print(ele+" ");
         }
-
         // Sort the array
         Arrays.sort(arr);
         System.out.println();
