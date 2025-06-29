43032ca1056200e4601def86a2e5bc126bbd09af
Ravi Raj <msd7ravis@gmail.com>
2025-06-29 16:47:40 +0530

    update

--- Arrays/MaximumNumber.java
+++ Arrays/MaximumNumber.java
@@ -2,7 +2,7 @@ package Arrays;
 
 public class MaximumNumber {
     public static void main(String[] args) {
-        int[] arr = {2, 3, 4, 5, 6, 7, 8};
+        int[] arr = {2, 3, 9, 5, 6, 7, 8};
         int max = 2;  //if take -1 then ot not run proper
 
         for (int i = 0; i < arr.length; i++) {
