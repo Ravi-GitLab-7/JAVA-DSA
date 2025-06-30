fb377921d1d8528295e2960a0b9b2b7bcc989018
Ravi Raj <msd7ravis@gmail.com>
2025-06-30 23:41:56 +0530

    update

--- Arrays/SecondLargestNum.java
+++ Arrays/SecondLargestNum.java
@@ -4,14 +4,12 @@ public class SecondLargestNum {
     public static void main(String[] args) {
         int[] arr = {2, 3, 4,7,9, 5, 6, 7, 8};
         int max = Integer.MIN_VALUE;  //if take -1 then ot not run proper
-
         for (int i = 0; i < arr.length; i++) {
             if (max < arr[i]) {
                 max = arr[i];
             }
         }
         int smax = Integer.MIN_VALUE;
-
         for (int i = 0; i < arr.length ; i++) {
             if(arr[i]>smax && arr[i]!=max){
                 smax = arr[i];
