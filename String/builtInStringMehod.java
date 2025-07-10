7c206c55a607a42d0fc0f387b457f46a1eed5db2
Ravi Raj <msd7ravis@gmail.com>
2025-07-10 12:20:35 +0530

    builtInStringMehod

---
+++ String/builtInStringMehod.java
@@ -0,0 +1,10 @@
+package String;
+public class builtInStringMehod {
+    public static void main(String[] args) {
+        String X = "Ravi";
+        int n = X.length();
+//        System.out.println(X.length());
+        System.out.println(X.charAt(3));
+        System.out.println(n);
+    }
+}
