41baf96c5b07d3173102f6911e0fe899160ec87d
Ravi Raj <msd7ravis@gmail.com>
2025-07-14 00:53:55 +0530

    returnNumberOfString

---
+++ String/interning.java
@@ -0,0 +1,8 @@
+package String;
+public class interning {
+    public static void main(String[] args) {
+        String s = "Ravi";
+//        s = s.charAt(1)= 'i';     these will give error
+
+    }
+}
