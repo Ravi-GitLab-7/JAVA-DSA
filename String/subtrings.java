9d2f8881f782884e32be1b452ced2c3cdcd30c0c
Ravi Raj <msd7ravis@gmail.com>
2025-07-13 23:27:47 +0530

    subtrings

---
+++ String/subtrings.java
@@ -0,0 +1,12 @@
+package String;
+
+public class subtrings {
+    public static void main(String[] args) {
+        String x = "abcde";
+        System.out.println(x.substring(2)); // index of 2 and uske aage wala
+        System.out.println(x.substring(2,4)); //i to j-1
+        System.out.println(x.substring(2,2));
+        System.out.println(x.substring(2,3));
+        System.out.println(x.substring(0,5));
+    }
+}
