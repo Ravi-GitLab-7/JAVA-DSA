e06c173188628f6b307d32a5886d9274232a3b49
Ravi Raj <msd7ravis@gmail.com>
2025-07-20 15:16:29 +0530

    StringEqualityTest

--- String/StringEqualityTest.java
+++ String/StringEqualityTest.java
@@ -1,15 +1,15 @@
 package String;
-public class equals {
+public class StringEqualityTest {
     public static void main(String[] args) {
         String a = "abcdxyz";
         String b = "abcdxyz";
-        String c = new String(a); // create new String object
+        String c = new String(a); // creates new String object
         String d = "abc";
-        d += "xyz";  // creates a new String
+        d = d + "xyz";  // creates a new String
 
-        System.out.println(a == b);      // true, both point to same string in the string pool
-        System.out.println(a == c);      // false, c is a new object
-        System.out.println(a == d);      // false, d is created at runtime
-        System.out.println(a.equals(d)); // true, content is same
+        System.out.println(a == b);      // true
+        System.out.println(a == c);      // false
+        System.out.println(a == d);      // false
+        System.out.println(a.equals(d)); // true
     }
 }
