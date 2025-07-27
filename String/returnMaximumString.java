e7d7859081d2b0a3d3d928c89559b0c8497741b3
Ravi Raj <msd7ravis@gmail.com>
2025-07-27 14:51:58 +0530

    returnMaximumString

---
+++ String/returnMaximumString.java
@@ -0,0 +1,13 @@
+package String;
+public class returnMaximumString {
+    public static void main(String[] args) {
+        String[] arr = {"999", "998", "997", "996", "995", "994", "993", "992", "991", "990"};  // string array
+        int mx = Integer.parseInt(arr[0]);{
+            for(int i= 1;i<arr.length;i++){
+                int n = Integer.parseInt(arr[i]);  // change in integer from string
+                mx = Math.max(mx,n);
+            }
+            System.out.println(mx);
+        }
+    }
+}
