b52e707896e653163c4bc240ed8096cb75302278
msd7r <msd7ravis@gmail.com>
2025-08-29 23:16:33 +0530

    public class checkIfArrayShortOrNot {

---
+++ basicSorting/checkIfArrayShortOrNot.java
@@ -0,0 +1,16 @@
+package basicSorting;
+public class checkIfArrayShortOrNot {
+    public static void main(String[] args) {
+        int [] arr = {1,2,4,5,20,6,34};
+        int n = arr.length;
+        boolean flag = true;
+        for (int i = 0; i <n-1; i++) {
+            if(arr[i]>arr[i+1]){
+                flag = false;
+                break;
+            }
+        }
+        if(flag==true) System.out.println("Soreted array");
+        else System.out.println("Unsorted array");
+    }
+}
