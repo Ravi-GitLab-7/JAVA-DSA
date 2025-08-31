19442bcfb1401099440b9a55321e397cfc6b206e
msd7r <msd7ravis@gmail.com>
2025-08-31 16:38:58 +0530

    bubbleSort2Optimize

--- basicSorting/bubbleSort2.java
+++ basicSorting/bubbleSort2.java
@@ -1,19 +1,20 @@
 package basicSorting;
-
-public class bubbleSort {
+public class bubbleSort2 {
     public static void print(int [] arr){
         for(int ele : arr){
             System.out.print(ele+" ");
         }
     }
+
     public static void main(String[] args) {
         int [] arr = {390,1,26,5,42};
         int n = arr.length;
         print(arr);
         System.out.println();
-        //bubble sort -> 1
+
+        //bubble sort => 2
         for(int x =0;x<n-1;x++){  //for passing  n-1 paases
-            for (int i = 0; i <n-1 ; i++) {
+            for (int i = 0; i <n-1-x ; i++) {
                 if(arr[i]>arr[i+1]){
                     int temp = arr[i];
                     arr[i]= arr[i+1];
--- basicSorting/bubbleSort2Optimize.java
+++ basicSorting/bubbleSort2Optimize.java
@@ -1,25 +1,29 @@
 package basicSorting;
-
-public class bubbleSort {
+public class bubbleSort2Optimize {
     public static void print(int [] arr){
         for(int ele : arr){
             System.out.print(ele+" ");
         }
     }
+
     public static void main(String[] args) {
         int [] arr = {390,1,26,5,42};
         int n = arr.length;
         print(arr);
         System.out.println();
-        //bubble sort -> 1
+
+        //bubble sort => 2
+        boolean flag = true;
         for(int x =0;x<n-1;x++){  //for passing  n-1 paases
-            for (int i = 0; i <n-1 ; i++) {
+            for (int i = 0; i <n-1-x ; i++) {
                 if(arr[i]>arr[i+1]){
                     int temp = arr[i];
                     arr[i]= arr[i+1];
                     arr[i+1]=temp;
+                    flag = true;
                 }
             }
+            if(flag= true) break;
         }
         print(arr);
     }
