0cdccdd74eeca35ab37cc86d2e0289a09321044e
msd7r <msd7ravis@gmail.com>
2025-09-04 00:39:59 +0530

    majorityElement

--- basicSorting/majorityElement.java
+++ basicSorting/majorityElement.java
@@ -1,32 +1,21 @@
 package basicSorting;
-public class insertion {
+public class majorityElement {
     public static void print(int[] arr) {
         for (int ele : arr) {
             System.out.print(ele + " ");
         }
     }
-
     public static void swap(int[] arr, int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
-
     public static void main(String[] args) {
-        int[] arr = {390, 1, 26, 5, 42};
+        int [] arr = {1,1,1,2,6,6,6,3,3,3,2,4,4,4,4,4,4,4};
         int n = arr.length;
         print(arr);
         System.out.println();
 
-        //insertion sort ->1
-//        for(int i =1;i<n;i++){  //for passing n-1 passes
-//            for (int j = i; j >=1 ; j--) {
-//                if(arr[j]<arr[j-1]){
-//                   swap(arr,j,j-1);
-//                }
-//                else break;
-//            }
-//        }
         for (int i = 1; i < n; i++) {  //for passing n-1 passes
             int j = i;
             while (j > 0 && arr[j] < arr[j - 1]){
