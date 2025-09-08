3070a9b488626d5342fed0ddfc68c984c6732119
msd7r <msd7ravis@gmail.com>
2025-09-09 00:48:32 +0530

    lowerBond

--- binarySearch/lowerBond.java
+++ binarySearch/lowerBond.java
@@ -1,9 +1,20 @@
-package binarySearch;
+package basicSorting;
 
 public class lowerBond {
     public static void main(String[] args) {
-        int [] arr = {12,34,56,68,98,45,78,56,34,58};
+        int[] arr ={13,45,56,56,23,86,45,67,90};
         int n = arr.length;
-
+        int lo = 0 , hi = n-1;
+        int target = 60;
+        int lb = n;
+        while(lo<=hi){
+            int mid = lo +(hi=lo)/2;
+            if(arr[mid]>target) {
+                lb = Math.min(lb, mid); // always write mid
+                hi = mid-1;
+            }
+            else lo= mid+1;
+        }
+        System.out.println(lb);
     }
 }
