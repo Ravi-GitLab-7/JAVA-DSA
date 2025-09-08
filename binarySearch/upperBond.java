debe7de4c8fbccd8e8339001fb969d84fe3319ff
msd7r <msd7ravis@gmail.com>
2025-09-09 01:15:22 +0530

    upperBond

--- binarySearch/upperBond.java
+++ binarySearch/upperBond.java
@@ -1,20 +1,19 @@
 package basicSorting;
-
-public class lowerBond {
+public class upperBond {
     public static void main(String[] args) {
         int[] arr ={13,45,56,56,23,86,45,67,90};
         int n = arr.length;
         int lo = 0 , hi = n-1;
         int target = 60;
-        int lb = n;
+        int ub = n;
         while(lo<=hi){
             int mid = lo +(hi=lo)/2;
-            if(arr[mid]>=target) {
-                lb = Math.min(lb, mid); // always write mid
+            if(arr[mid]>target) {
+                ub = Math.min(ub, mid); // always write mid
                 hi = mid-1;
             }
             else lo= mid+1;
         }
-        System.out.println(lb);
+        System.out.println(ub);
     }
 }
