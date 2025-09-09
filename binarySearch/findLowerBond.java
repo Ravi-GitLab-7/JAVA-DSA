d63fe3114a46d94ab56d445bf2430427955de7ea
msd7r <msd7ravis@gmail.com>
2025-09-09 09:08:49 +0530

    findLowerBond

---
+++ binarySearch/findLowerBond.java
@@ -0,0 +1,19 @@
+package binarySearch;
+public class findLowerBond {
+    public static void main(String[] args) {
+        int [] arr = {4,6,10,12,18,18,20,20,30,46};
+        int n = arr.length;
+        int lb = n;
+        int lo=0,hi=n-1;
+        int target = 18;
+        while(lo<=hi){
+            int mid = lo+(hi-lo)/2;
+            if(arr[mid]>=target){
+                lb=Math.min(lb, mid);
+                hi = mid - 1;
+            }
+            else lo= mid+1;
+        }
+        System.out.println(lb);
+    }
+}
