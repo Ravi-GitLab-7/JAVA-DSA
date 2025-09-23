362c90e772ebcde710f19543d0958dcec01b4a2e
msd7r <msd7ravis@gmail.com>
2025-09-23 19:52:11 +0530

    updates

--- binarySearch/searchRotedArray.java
+++ binarySearch/searchRotedArray.java
@@ -1,6 +1,40 @@
-package binarySearch;
-public class searchRotedArray {
+class SearchRotedArray {
+    public int search(int[] nums, int target) {
+        int lo = 0, hi = nums.length - 1;
+
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+
+            if (nums[mid] == target) return mid;
+
+            // Right half is sorted
+            if (nums[mid] < nums[hi]) {   // strictly <, not <=
+                if (target > nums[mid] && target <= nums[hi]) {
+                    lo = mid + 1;
+                } else {
+                    hi = mid - 1;
+                }
+            }
+            // Left half is sorted
+            else {
+                if (target >= nums[lo] && target < nums[mid]) {
+                    hi = mid - 1;
+                } else {
+                    lo = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
-        
+        SearchRotedArray sol = new SearchRotedArray();
+
+        int[] nums1 = {4,5,6,7,0,1,2};
+        System.out.println(sol.search(nums1, 0)); // Output: 4
+        System.out.println(sol.search(nums1, 3)); // Output: -1
+
+        int[] nums2 = {1};
+        System.out.println(sol.search(nums2, 0)); // Output: -1
     }
 }
