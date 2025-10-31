333e2966bd9be972f650f482540d6d04bcbe72a1
Ravi Raj <msd7ravis@gmail.com>
2025-10-31 23:59:26 +0530

    sortlinear

--- Advance_sorting/cyclesort.java
+++ Advance_sorting/cyclesort.java
@@ -1,8 +1,32 @@
 package Advance_sorting;
-
 public class cyclesort {
+    public static void print(int []arr){
+        for(int ele: arr){
+            System.out.print(ele+" ");
+        }
+    }
+    public static void swap(int i,int j,int[]arr){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    // 2️⃣ missingnum function corrected minimal changes
+    public static void missingnum(int[]arr){
+        int n = arr.length;
+        int i = 0;
+        while(i < n){
+            if(arr[i] == i || arr[i] == n){
+                i++;
+            } else {
+                swap(i, arr[i], arr);
+            }
+        }
+    }
     public static void main(String[] args) {
-
-
+        int [] arr = {2,5,3,1,0,7,9,4};
+        missingnum(arr);
+        // Just to show final array
+        print(arr);
     }
 }
