1cf83c235d28d17e3c60923ce7870101ae943a01
Ravi Raj <msd7ravis@gmail.com>
2025-11-01 01:40:19 +0530

    duplicatelement

---
+++ Advance_sorting/cyclesort/duplicatelement.java
@@ -0,0 +1,16 @@
+package Advance_sorting.cyclesort;
+public class duplicatelement {
+    public static void main(String[] args) {
+        int [] arr = {3,3,1};
+        int n = arr.length+1;
+        // sum of numbers from 0 to n-1
+        int totalSum = (n * (n - 1)) / 2;
+        // Sum of given array
+        int arrSum = 0;
+        for (int x : arr) {
+            arrSum += x;
+        }
+        int result = arrSum - totalSum;
+        System.out.println("Duplicate number is: " + result);
+    }
+}
