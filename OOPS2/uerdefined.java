0f150925c223dad4ac9baea3c241bc2ebc214aa6
Ravi Raj <msd7ravis@gmail.com>
2025-12-08 23:59:51 +0530

    oops

---
+++ OOPS2/uerdefined.java
@@ -0,0 +1,28 @@
+package OOPS2;
+import com.sun.security.jgss.GSSUtil;
+public class uerdefined {
+    public static class Student {  // this dataa types
+        String name ;
+        int roln;
+        double cgpa;
+    }
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name = "Ravi Raj";
+        s1.roln = 249026;
+        s1.cgpa = 8.5;
+
+        Student s2 = new Student();
+        s2.name = "Hari thapa";
+        s2.roln = 249146;
+        s2.cgpa = 8.9;
+
+        Student s3 = new Student();
+        s3.name = "Rahul kumar";
+        s3.roln = 249027;
+        s3.cgpa = 9.5;
+
+        System.out.println(s1.name);
+        System.out.println(s3.cgpa);
+    }
+}
