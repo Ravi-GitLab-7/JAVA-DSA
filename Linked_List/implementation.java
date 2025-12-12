3ed2ce1b76ad2c01eeb9f550a97e8c204df94931
Ravi Raj <msd7ravis@gmail.com>
2025-12-12 19:36:56 +0530

    inserAtHeead

--- Linked_List/implementation.java
+++ Linked_List/implementation.java
@@ -1,11 +1,10 @@
 package Linked_List;
-
-class SLL{
+class SLL {
     private Node head;
     private Node tail;
     private int size = 0;
 
-    // insert new value
+    // insert new value at end
     void inserAtEnd(int val){
         Node temp = new Node(val);
         if(head == null){
@@ -18,7 +17,8 @@ class SLL{
         size++;   // increase length
     }
 
-    void inserAtHeead(int val){
+    // insert at head
+    void inserAtHead(int val){
         Node temp = new Node(val);
         if(head == null){
             head = tail = temp;
@@ -29,8 +29,42 @@ class SLL{
         }
         size++;   // increase length
     }
+    void inseertElement(int idx,int val){
+        if(idx == 0){
+            inserAtHead(val);
+            return;
+        }
+        if(idx == size){
+            inserAtEnd(val);
+            return;
+        }
+        if(idx > size || idx < 0){
+            System.out.println("Invalid!!");
+        }
+        Node temp = new Node(val);
+        Node x = head;
+        for (int i = 0; i <idx - 1 ; i++) {
+            x = x.next;
+        }
+        temp.next = x.next;
+        x.next = temp;
+        size ++;
+    }
+    int get(int idx){
+        if(idx == size) return tail.val;
+        if(idx >= size || idx < 0){
+            System.out.println("Invalid");
+            return -1;
+        }
+        Node temp = head;
+        for (int i = 0; i <idx ; i++) {
+            temp = temp.next;
+        }
+        return temp.val;
+    }
 
-    void display(){   // print by iteration method
+    // display linked list
+    void display(){
         Node temp = head;
         while(temp != null){
             System.out.print(temp.val + " ");
@@ -39,26 +73,30 @@ class SLL{
         System.out.println();
     }
 
+    // display size of linked list
     void Size(){
         System.out.println("Length of Linked list is: " + size);
     }
 }
+
 public class implementation {
     public static void main(String[] args) {
         SLL list = new SLL();
-        list.Size();        // prints 0
+        list.Size();           // prints 0
         list.inserAtEnd(10);
         list.inserAtEnd(20);
-        list.display();
+        list.display();        // 10 20
         list.inserAtEnd(30);
         list.inserAtEnd(40);
-        list.Size();
+        list.Size();           // prints 4
+        list.display();        // 10 20 30 40
+        list.inserAtHead(10);
+        list.inserAtHead(20);
+        list.inserAtHead(30);
+        list.display();        // 30 20 10 10 20 30 40
+        list.inseertElement(3,100);
         list.display();
 
-        list.inserAtHeead(10);
-        list.inserAtHeead(20);
-        list.inserAtHeead(30);
-        list.display();
-
+        System.out.println(list.get(6));
     }
 }
---
+++ Linked_List/insertElement.java
@@ -0,0 +1,58 @@
+//package Linked_List;
+//class SLL {
+//    private Node head;
+//    private Node tail;
+//    private int size = 0;
+//
+//    // insert at head
+//    void inserAtHead(int val){
+//        Node temp = new Node(val);
+//        if(head == null){
+//            head = tail = temp;
+//        }
+//        else {
+//            temp.next = head;
+//            head = temp;
+//        }
+//        size++;   // increase length
+//    }
+//    void inseertElement(int idx,int val){
+//        Node temp = new Node(val);
+//        Node x = head;
+//        for (int i = 0; i <idx - 1 ; i++) {
+//            x = x.next;
+//        }
+//        temp.next = x.next;
+//        x.next = temp;
+//        size ++;
+//    }
+//
+//    // display linked list
+//    void display(){
+//        Node temp = head;
+//        while(temp != null){
+//            System.out.print(temp.val + " ");
+//            temp = temp.next;
+//        }
+//        System.out.println();
+//    }
+//
+//    // display size of linked list
+//    void Size(){
+//        System.out.println("Length of Linked list is: " + size);
+//    }
+//}
+//
+//public class insertElement {
+//    public static void main(String[] args) {
+//        SLL list = new SLL();
+//        list.Size();           // prints 0
+//        list.display();        // 10 20 30 40
+//        list.inserAtHead(10);
+//        list.inserAtHead(20);
+//        list.inserAtHead(30);
+//        list.display();        // 30 20 10 10 20 30 40
+//        list.inseertElement(3,100);
+//        list.display();
+//    }
+//}
