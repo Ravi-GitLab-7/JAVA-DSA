52a42a5c4718b76862aa77e862af7d0421e4c75c
Ravi Raj <msd7ravis@gmail.com>
2026-04-02 22:53:38 +0530

    Doubly linkedList

--- Linked_List/doublyLinkedListImplementation.java
+++ Linked_List/doublyLinkedListImplementation.java
@@ -10,33 +10,83 @@ class dNode{
 }
 // user defined data stracture
 class DLL{
-    
-}
-public class doublyLinkedListImplementation {
-    public static void print(dNode head){
+    dNode head;
+    dNode tail;
+    int size;
+    void display(){
         dNode temp = head;
-        while(temp!=null){
+        while(temp != null){
             System.out.print(temp.VAl+" ");
             temp= temp.next;
         }
         System.out.println();
     }
-    public static void  printReverse(dNode tail){
-        dNode temp = tail;
-        while(temp!=null){
-            System.out.print(temp.VAl+" ");
-            temp= temp.prev;
+    void insrtAtEnd(int Val){
+        dNode temp = new dNode(Val);
+        if(size == 0) head = tail = temp;
+        else {
+            tail.next = temp;
+            temp.prev = tail;
+            tail = temp;
         }
-        System.out.println();
+        size++;
     }
-    public static void  display(dNode node){
-        dNode temp = node;
-        while(temp.prev!=null){
-            temp = temp.prev;
+    void inserAtHead(int Val){
+        dNode temp = new dNode(Val);
+        if(size == 0) head = tail = temp;
+        else {
+            temp.next = head;
+            head.prev = temp;
+            head = temp;
         }
-            // now it reach to head
-        print(temp);
+        size++;
     }
+}
+
+public class doublyLinkedListImplementation {
+//    public static void print(dNode head){
+//        dNode temp = head;
+//        while(temp!=null){
+//            System.out.print(temp.VAl+" ");
+//            temp= temp.next;
+//        }
+//        System.out.println();
+//    }
+//    public static void  printReverse(dNode tail){
+//        dNode temp = tail;
+//        while(temp!=null){
+//            System.out.print(temp.VAl+" ");
+//            temp= temp.prev;
+//        }
+//        System.out.println();
+//    }
+//    public static void  display(dNode node){
+//        dNode temp = node;
+//        while(temp.prev!=null){
+//            temp = temp.prev;
+//        }
+//            // now it reach to head
+//        print(temp);
+//    }
+//    public boolean isPallindrome(dNode head){
+//            if(head == null && head.next == null){
+//                return true;
+//            }
+//            // find tail
+//        dNode tail = head;
+//            while (tail.next!= null){
+//                tail = tail.next;
+//            }
+//            // compare both
+//        while(head!= tail && head.prev != tail){
+//            if(head.VAl != tail.VAl){
+//                return false;
+//            }
+//            head = head.next;
+//            tail = tail.prev;
+//        }
+//        return  true;
+//    }
     public static void main(String[] args) {
 //        dNode a = new dNode(10);
 //        dNode b = new dNode(20);
@@ -48,11 +98,23 @@ public class doublyLinkedListImplementation {
 //        b.next = c; c.prev = b;
 //        c.next = d; d.prev = c;
 //        d.next = e; e.prev = d;
-//
+
 //        print(a);
 //        printReverse(e);
 //        // now give any random dll then we print whole ll
 //        display(c);
+//        doublyLinkedListImplementation obj = new doublyLinkedListImplementation();
+//        System.out.println(obj.isPallindrome(a));
         DLL list = new DLL();
+        list.display();
+        list.insrtAtEnd(10);
+        list.insrtAtEnd(20);
+        list.insrtAtEnd(30);
+        list.display();
+        list.insrtAtEnd(40);
+        list.display();
+        System.out.println(list.size);
+        list.inserAtHead(60);
+        list.display();
     }
 }
