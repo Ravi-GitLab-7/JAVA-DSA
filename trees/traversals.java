8a91dc50b8df5aa7e1edaa717e40f871099930a8
Ravi Raj <msd7ravis@gmail.com>
2026-09-06 16:32:23 +0530

    new

--- trees/traversals.java
+++ trees/traversals.java
@@ -3,15 +3,15 @@ import java.util.LinkedList;
 import java.util.Queue;
 public class traversals {
     public static void main(String[] args) {
-        Node a = new Node(2);
-        Node b = new Node(3);
-        Node c = new Node(4);
-        Node d = new Node(7);
-        Node e = new Node(8);
-        Node f = new Node(9);
-        Node g = new Node(10);
-        Node h = new Node(11);
-        Node i = new Node(12);
+        Node a = new Node(1);
+        Node b = new Node(2);
+        Node c = new Node(3);
+        Node d = new Node(4);
+        Node e = new Node(5);
+        Node f = new Node(6);
+        Node g = new Node(7);
+        Node h = new Node(8);
+        Node i = new Node(9);
         a.left = b ; a.right = c;
         b.left = d ; b.right = e;
         c.left = f ; c.right = g;
@@ -32,10 +32,10 @@ public class traversals {
         while (q.size() > 0) {
             Node front = q.remove();
             System.out.print(front.val + " ");
-            if (front.left != null)
-                q.add(front.left);
             if (front.right != null)
                 q.add(front.right);
+            if (front.left != null)
+                q.add(front.left);
         }
     }
     public static void preorder(Node root){
