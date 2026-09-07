f9954aa315cd658fc0e2576a1bd94320502d173a
Ravi Raj <msd7ravis@gmail.com>
2026-09-07 09:43:15 +0530

    new

--- trees/traversals.java
+++ trees/traversals.java
@@ -1,8 +1,22 @@
 package trees;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Scanner;
+
 public class traversals {
+    // Pair class for levelOrder()
+    static class Pair {
+        Node node;
+        int level;
+
+        Pair(Node node, int level) {
+            this.node = node;
+            this.level = level;
+        }
+    }
+    static int n ;
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Node a = new Node(1);
         Node b = new Node(2);
         Node c = new Node(3);
@@ -16,28 +30,61 @@ public class traversals {
         b.left = d ; b.right = e;
         c.left = f ; c.right = g;
         e.left = h ; f.right = i;
-        System.out.print("preorder"+" ");
+        System.out.println("preorder"+" ");
         preorder(a);
-        System.out.print("inorder"+" ");
+        System.out.println("inorder"+" ");
         inorder(a);
-        System.out.print("postorder"+" ");
+        System.out.println("postorder"+" ");
         postorder(a);
         System.out.println();
-        System.out.print("Level Order: ");
-        levelOrder(a);
-    }
-    private static void levelOrder(Node root) {
-        Queue<Node> q = new LinkedList<>();
-        if (root != null) q.add(root);
-        while (q.size() > 0) {
-            Node front = q.remove();
-            System.out.print(front.val + " ");
-            if (front.right != null)
-                q.add(front.right);
-            if (front.left != null)
-                q.add(front.left);
+//        System.out.print("Level Order: ");
+//        levelOrder(a);
+//        System.out.print("Enter n: ");
+//        n = sc.nextInt();
+        for(int x = 0;x<3;x++){
+            n = x;
+            nThLevel(a,0);
+            System.out.println();
         }
     }
+    private static void levelOrder(Node root) {
+        int prevLevel = 0;
+        Queue<Pair> q = new LinkedList<>();
+        if (root != null)
+            q.add(new Pair(root, 0));
+        while (q.size() > 0) {
+            Pair front = q.remove();
+            Node temp = front.node;
+            int lvl = front.level;
+            if (lvl != prevLevel) {
+                System.out.println();
+                prevLevel++;
+            }
+            System.out.print(temp.val + " ");
+            if (temp.left != null)
+                q.add(new Pair(temp.left, lvl + 1));
+            if (temp.right != null)
+                q.add(new Pair(temp.right, lvl + 1));
+        }
+    }
+    public static void nThLevel(Node root,int level){
+        if(root==null) return;
+        if(level==n) System.out.print(root.val+" ");
+        nThLevel(root.left,level+1);
+        nThLevel(root.right,level+1);
+    }
+//    private static void levelOrder(Node root) {
+//        Queue<Node> q = new LinkedList<>();
+//        if (root != null) q.add(root);
+//        while (q.size() > 0) {
+//            Node front = q.remove();
+//            System.out.print(front.val + " ");
+//            if (front.right != null)
+//                q.add(front.right);
+//            if (front.left != null)
+//                q.add(front.left);
+//        }
+//    }
     public static void preorder(Node root){
         if(root==null) return;
         System.out.print(root.val+" ");
